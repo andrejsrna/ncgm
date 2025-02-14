448b8af131eac7949138f8ff779b2f7ad3d518c9
Andrej Srna <andrejsrna@MacBook-Pro-uzivatela-Andrej.local>
2025-02-14 21:21:30 +0100

    fix

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "./ui/button";
 import Link from 'next/link';
-import { FaPlay, FaMusic, FaArrowRight } from 'react-icons/fa';
+import { FaMusic } from 'react-icons/fa';
 
 export default function Hero() {
   return (
